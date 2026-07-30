7a03e9410384c390e4c25934ba814fddc53f67ce
AlessxR <alexxsrs.al@gmail.com>
2026-07-30 15:50:38 +0300

    add error, isError, reply response

--- src/components/PostsPage.tsx
+++ src/components/PostsPage.tsx
@@ -7,34 +7,34 @@ type Post = {
     title: string;
 };
 
+let retryCount = 0;
+
 function getPosts() {
+    retryCount++;
+    console.log(retryCount);
+    if (true) {
+        throw new Error('TEST ERROR');
+    }
     return api.get<Post[]>('/posts').then((res) => res.data);
 }
 
 export const PostsPage = () => {
     const {
         data: posts,
-        isLoading,
-        isPending,
-        isFetching,
-        status,
-        fetchStatus,
+        error,
+        isError,
     } = useQuery({
         queryKey: ['posts'],
         queryFn: getPosts,
-        staleTime: 30000, // 30 секунд, в течение которых данные считаются актуальными и не будут повторно запрашиваться
-        gcTime: 60000, // 60 секунд, и данные будут удалены из кэша, ибо они больше не актуальны
+        retry: 1,
     });
 
     console.log(posts);
 
     return (
         <div className="flex flex-col gap-4">
-            {status}
-            {fetchStatus}
-            {isLoading && <div>Loading...</div>}
-            {isPending && <div>Pending...</div>}
-            {isFetching && <div>Fetching...</div>}
+            {isError && <div>Произошла ошибка: {error.message}</div>}
+            {retryCount}
             {posts?.map((post) => (
                 <div key={post.id}>
                     {post.id}
